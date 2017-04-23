4e8bf0450fc870a4e364be4a1ee9a1d1063c0680
zhuHuaJian <935625755@qq>
2017-04-23 13:49:47 +0800

    zengjia houtai zidong gengxin tianqi gongeng

--- app/src/main/java/com/example/administrator/coolweather/service/AutoUpdateService.java
+++ app/src/main/java/com/example/administrator/coolweather/service/AutoUpdateService.java
@@ -1,9 +1,12 @@
 package com.example.administrator.coolweather.service;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
@@ -32,6 +35,14 @@ public class AutoUpdateService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         updateWeather();
+        updateBingPic();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 60 * 60 * 1000;//一小时更新一次
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
         return super.onStartCommand(intent, flags, startId);
 
     }
