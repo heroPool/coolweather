66ee37e9eee11dd4051cef1280cc2c5eedd10664
zhuHuaJian <935625755@qq>
2017-05-18 17:25:11 +0800

    some modification

--- app/src/main/java/com/example/administrator/coolweather/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.coolweather;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction fragmentTran = getSupportFragmentManager().beginTransaction();
+        fragmentTran.replace(R.id.layout_content, new ChooseAreaFragment());
+        fragmentTran.commit();
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getString("weather", null) != null) {
             Intent intent = new Intent(this, WeatherActivity.class);
